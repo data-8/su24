9034b460e273f0bdc6c586ac4485ac00969333ed
Jonathan Ferrari <jonathanferrari@berkeley.edu>
2024-06-18 13:36:33 -0700

    Update parse_issue.py

--- parse_issue.py
+++ parse_issue.py
@@ -7,6 +7,7 @@ lecture_number = re.search(r'Lecture Number: (\d+)', issue_body).group(1).strip(
 demo = re.search(r'Demo: (.+)', issue_body).group(1).strip()
 slides = re.search(r'Slides: (.+)', issue_body).group(1).strip()
 
-print(f"::set-env name=Lecture_Number::{lecture_number}")
-print(f"::set-env name=Demo::{demo}")
-print(f"::set-env name=Slides::{slides}")
+with open(os.getenv('GITHUB_ENV'), 'a') as env_file:
+    env_file.write(f"Lecture_Number={lecture_number}\n")
+    env_file.write(f"Demo={demo}\n")
+    env_file.write(f"Slides={slides}\n")
